418770e10166a04937dcb0f2f75df5a9b02b5506
assafsauer <as.sauer@gmail.com>
2021-06-03 11:35:15 +0200

    Update create.istio.logs.sh

--- Auth-policy/create.istio.logs.sh
+++ Auth-policy/create.istio.logs.sh
@@ -9,4 +9,4 @@ kubectl get pods -n $namespace |  awk '{print $1}' | grep -v NAME > pods.list
 while IFS= read -r line; do  echo $line; kubectl logs $line  -c istio-proxy -n  $namespace  >>  logs.istio; let "a++"; done < pods.list
 
 #  convert to CSV
-cat logs.istio | tr -s '[:blank:]' ',' |grep 'POST\|GETT\|HEADT\|PATCHT\|DELETE' |grep -v PassthroughCluster  > logs.csv
+cat logs.istio | tr -s '[:blank:]' ',' |grep 'POST\|GET\|HEADT\|PATCHT\|DELETE' |grep -v PassthroughCluster  > logs.csv
